9e5e01c56e544485db54fe54c1eecc69c17451b6
Michael Bunte <michaelwbunte@gmail.com>
2024-01-20 02:34:11 -0800

    Form values handled in state, form validation

--- webtools-client/src/App.js
+++ webtools-client/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 const font_family = "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif";
 const input_style = {
@@ -14,14 +14,22 @@ const input_style = {
 
 function InputComponent({
   upper_text,
-  lower_text
+  lower_text,
+  on_change = () => { },
+  inner_value,
+  min = 0,
+  max = 10
 }) {
+
+  const is_valid = parseInt(inner_value) >= min && parseInt(inner_value) <= max;
+  const error_style = is_valid || inner_value === "" ? { color: "rgba(0,0,0,0)" } : { color: "#ff6868" };
+
   return (
     <div style={{
       display: "flex",
       flexDirection: "column",
       alignItems: "center",
-      padding: "10px"
+      padding: "0px"
     }}>
       <div style={{
         fontSize: "1.2rem",
@@ -33,7 +41,8 @@ function InputComponent({
         padding: "5px"
       }}>
         <input style={input_style}
-          type="text" name="name" />
+          type="text" name="name" onChange={on_change} value={inner_value}
+        />
       </div>
       <div style={{
         color: "#ffa200",
@@ -41,11 +50,15 @@ function InputComponent({
       >
         {lower_text}
       </div>
+      <div style={error_style}>Invalid Input</div>
     </div>
   )
 }
 
-function MembraneSelect() {
+function MembraneSelect({
+  inner_value,
+  on_change = () => { }
+}) {
   return (
     <div style={{
       display: "flex",
@@ -62,7 +75,7 @@ function MembraneSelect() {
       <div style={{
         padding: "5px"
       }}>
-        <select value="ZZZ" style={input_style}>
+        <select value={inner_value} style={input_style} onChange={on_change}>
           <option value="CSM RE4040BE">CSM RE4040BE</option>
           <option value="HID RO4040HF">HID RO4040HF</option>
         </select>
@@ -126,6 +139,26 @@ function TextTopComponent({ outer_text, inner_text }) {
 
 
 function App() {
+  const [form_values, set_form_values] = useState({
+    "temperature": { "value": "", "min": 15, "max": 25, "type": "text", },
+    "bulknitrate": { "value": "", "min": 27, "max": 185, "type": "text", },
+    "bulksalt": { "value": "", "min": 1450, "max": 4800, "type": "text", },
+    "transmembranepressure": { "value": "", "min": 160, "max": 230, "type": "text", },
+    "membranetype": { "value": "CSM RE4040BE", "type": "dropdown" }
+  });
+
+  function update_value(field_name) {
+    return (e) => {
+      const re = /^-?[0-9\b]*$/;
+      if (re.test(e.target.value) || e.target.value === "") {
+        set_form_values((old) => ({ ...old, [field_name]: { ...old[field_name], "value": e.target.value } }))
+      }
+    };
+  }
+
+  function query_database() {
+    // TODO
+  }
 
   const category_style = {
     color: "#ffa200",
@@ -139,8 +172,20 @@ function App() {
     flexDirection: "column",
     alignItems: "center",
     width: "340px",
-    // border: "10px red solid"
   };
+
+
+  // Coding like this isn't best practice but I really wanted to do this as 
+  // a one liner
+  // Sums up all of the invalid fields and checks if it's equal to 0
+  let all_fields_valid = Object.keys(form_values).map(
+    key => (form_values[key]["type"] === "text" &&
+      (parseInt(form_values[key]["value"]) > form_values[key]["max"] ||
+        parseInt(form_values[key]["value"]) < form_values[key]["min"] ||
+        Number.isNaN(parseInt(form_values[key]["value"])))
+    ) ? 1 : 0
+  ).reduce((x, y) => x + y, 0) === 0;
+
 
   const calculate_button_style = {
     marginTop: "20px",
@@ -152,8 +197,11 @@ function App() {
     fontSize: "1.4rem",
     borderRadius: "10px",
     fontWeight: "500",
-    fontFamily: font_family
+    fontFamily: font_family,
+    cursor: all_fields_valid ? "pointer" : "not-allowed",
+    opacity: all_fields_valid ? "1" : "0.4"
   }
+
 
   return (
     <div >
@@ -180,17 +228,46 @@ function App() {
             flexWrap: "wrap"
           }}
           >
-
             <div style={inner_column_style} >
               <div style={category_style}>
                 INPUT PARAMETERS
               </div>
-              <InputComponent upper_text="Temperature" lower_text="15-25°C" />
-              <InputComponent upper_text="Bulk Nitrate" lower_text="27-185 mg/L" />
-              <InputComponent upper_text="Bulk Salt" lower_text="1450-4800 mg/L" />
-              <InputComponent upper_text="Transmembrane Pressure" lower_text="160-230 PSI" />
-              <MembraneSelect />
-              <button style={calculate_button_style} onClick={()=>{console.log("clicked")}}>
+              <InputComponent
+                upper_text="Temperature" lower_text="15-25°C"
+                inner_value={form_values["temperature"]["value"]}
+                min={form_values["temperature"]["min"]}
+                max={form_values["temperature"]["max"]}
+                on_change={update_value("temperature")}
+              />
+              <InputComponent
+                upper_text="Bulk Nitrate" lower_text="27-185 mg/L"
+                inner_value={form_values["bulknitrate"]["value"]}
+                min={form_values["bulknitrate"]["min"]}
+                max={form_values["bulknitrate"]["max"]}
+                on_change={update_value("bulknitrate")}
+              />
+              <InputComponent
+                upper_text="Bulk Salt" lower_text="1450-4800 mg/L"
+                inner_value={form_values["bulksalt"]["value"]}
+                min={form_values["bulksalt"]["min"]}
+                max={form_values["bulksalt"]["max"]}
+                on_change={update_value("bulksalt")}
+              />
+              <InputComponent
+                upper_text="Transmembrane Pressure" lower_text="160-230 PSI"
+                inner_value={form_values["transmembranepressure"]["value"]}
+                min={form_values["transmembranepressure"]["min"]}
+                max={form_values["transmembranepressure"]["max"]}
+                on_change={update_value("transmembranepressure")}
+              />
+              <MembraneSelect
+                inner_value={form_values["membranetype"]["value"]}
+                on_change={(e) => { set_form_values((old) => ({ ...old, "membranetype": { ...old["membranetype"], "value": e.target.value } })) }}
+              />
+              <button
+                style={calculate_button_style}
+                onClick={all_fields_valid ? query_database : () => { }}
+              >
                 CALCULATE
               </button>
             </div>
